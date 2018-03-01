0171351b6554396145c15c5439da54525501100b
XanderVi <engineermexanic@gmail.com>
2018-03-01 16:45:32 +0200

    0.5 of the original

--- verification/tests.py
+++ verification/tests.py
@@ -10,68 +10,20 @@ Each test is a dict with
 
 TESTS = {
     "Basics": [
-        {
-            "input": ['somefile.txt', '*'],
-            "answer": True,
-            "explanation": "* matches everything"
-        },
-        {
-            "input": ['other.exe', '*'],
-            "answer": True,
-            "explanation": "* matches everything"
-        },
-        {
-            "input": ['my.exe', '*.txt'],
-            "answer": False,
-            "explanation": "filename should ends with \".txt\" but \".exe\" found "
-        },
-        {
-            "input": ['log1.txt', 'log?.txt'],
-            "answer": True,
-            "explanation": "? matches any single character (in this case it is \"1\""
-        },
+        
         {
             "input": ['log1.txt', 'log[1234567890].txt'],
             "answer": True,
             "explanation": "[seq] matches any character in seq. \"1\" is in \"1234567890\""
         },
         {
-            "input": ['log12.txt', 'log?.txt'],
+            "input": ['log1.txt', 'log[!1].txt'],
             "answer": False,
-            "explanation": "? matches any single character. Not more than one"
-        },
-        {
-            "input": ['log12.txt', 'log??.txt'],
-            "answer": True,
-            "explanation": "? matches any two character"
+            "explanation": "!1 matches any single character except 1"
         }
     ],
     "Extra": [
-        {
-            "input": ['log12.txt', '**'],
-            "answer": True,
-            "explanation": "** works the same as *"
-        },
-        {
-            "input": ['file19.txt', '*z*'],
-            "answer": False,
-            "explanation": "\"z\" should be somewhere inside the filename"
-        },
-        {
-            "input": ['l.txt', '???*'],
-            "answer": True,
-            "explanation": "Filename should be 3 chars long"
-        },
-        {
-            "input": ['txt', '????*'],
-            "answer": False,
-            "explanation": "Filename should be 4 chars long"
-        },
-        {
-            "input": ['name.txt', 'name.txt'],
-            "answer": True,
-            "explanation": "filename matches itself"
-        },
+        
         {
             "input": ['name.txt', 'name.exe'],
             "answer": False,
@@ -103,61 +55,21 @@ TESTS = {
             "explanation": "[!seq] matches any character not in seq"
         },
         {
-            "input": ['1name.txt', '[!1234567890]*'],
+            "input": ['1name.txt', '[!1234567890]name.txt'],
             "answer": False,
             "explanation": "filename should not start with number"
         },
         {
-            "input": ['name.txt', '[!1234567890]*'],
+            "input": ['name.txt', '[!1234567890]ame.txt'],
             "answer": True,
             "explanation": "filename should not start with number"
         },
         {
-            "input": ['a1name.txt', '[!1234567890]*'],
+            "input": ['a1name.txt', '[!1234567890]1name.txt'],
             "answer": True,
             "explanation": "filename should not start with number"
         },
-        {
-            "input": ['apache12.log', '*[1234567890].*'],
-            "answer": True,
-            "explanation": "should be a number before extension"
-        },
-        {
-            "input": ['apache.1log', '*[1234567890].*'],
-            "answer": False,
-            "explanation": "should be a number before extension"
-        },
-        {
-            "input": ['apache1.log', '*.*'],
-            "answer": True,
-            "explanation": "filename should have an extension"
-        },
-        {
-            "input": ['12apache1.log', '*.*'],
-            "answer": True,
-            "explanation": "filename should have an extension"
-        },
-        {
-            "input": ['12apache1', '*.*'],
-            "answer": False,
-            "explanation": "filename should have an extension"
-        },
-        {
-            "input": ['name.txt', 'name.???'],
-            "answer": True
-        },
-        {
-            "input": ['name.exe', 'name.???'],
-            "answer": True
-        },
-        {
-            "input": ['name....', 'name.???'],
-            "answer": True
-        },
-        {
-            "input": ['name....', 'name.*'],
-            "answer": True
-        },
+        
         {
             "input": ['name....', 'name.[!.][!.][!.]'],
             "answer": False
@@ -176,23 +88,11 @@ TESTS = {
             "answer": True,
             "explanation": "unmatched brackets"
         },
-        {
-            "input": ['[?*]', '[[][?][*][]]'],
-            "answer": True,
-            "explanation": "escaping metacharacters"
-        },
-        {
-            "input": ['Feb 2018', '[A-Z][a-z][a-zA-Z] [2-3][0-4][1-1][5-9]'],
-            "answer": True
-        },
-        {
-            "input": ['[check].txt', '[][]check[][].txt'],
-            "answer": True
-        },
+        
         {
             "input": ["checkio", "[c[]heckio"],
             "answer": True
-        },
+        }
 
     ]
 }
